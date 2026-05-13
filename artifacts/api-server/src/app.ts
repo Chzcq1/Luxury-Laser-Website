87665c5a7f88600a898d0d364d289738288f8a60
Shean <184720723+Chzcq1@users.noreply.github.com>
2026-05-13 06:13:37 +0000

    แก้ไขกันรันเซิร์ฟ

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,3 +1,4 @@
+import express, { type Express, Request, Response } from "express";
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
@@ -9,12 +10,15 @@ const app: Express = express();
 app.use(
   pinoHttp({
     logger,
+    req(req: Request) {
     serializers: {
       req(req) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
+        },
+    res(res: Response) {
         };
       },
       res(res) {
@@ -32,3 +36,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 export default app;
+},
